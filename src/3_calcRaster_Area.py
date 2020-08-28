1ef30862b56f381f231d8b0f4f5ef2a135052abe
Vinícius Mesquita <vieiramesquita@gmail.com>
2020-08-28 10:43:22 -0300

    Update and rename GetRasterAreas_v4_2_custom_v2_areaCalc.py to 3_calcRaster_Area.py

--- src/3_calcRaster_Area.py
+++ src/3_calcRaster_Area.py
@@ -121,94 +121,23 @@ def zonal_stats(feat, input_zone_polygon, input_value_raster, nb):
     for y in range(0,ycount,rowSize):
 
         if (y+rowSize) <= (ycount):
-            #print(ycount - y,ycount,y,y+rowSize)
-            #print(xcount,ycount)
-            #print(xoff, y+yoff,xcount, rowSize)
-            #dataraster = banddataraster.ReadAsArray(0, y, xcount,rowSize).astype(numpy.byte)
-            #datamask = bandmask.ReadAsArray(x-xoff, y-yoff,lineSize, rowSize).astype(numpy.byte)
-
             dataraster = banddataraster.ReadAsArray(xoff, y+yoff,xcount, rowSize).astype(numpy.byte)
             datamask = bandmask.ReadAsArray(0, y, xcount,rowSize).astype(numpy.byte)
             
         else:
-            #print('hu3')
-            #dataraster = banddataraster.ReadAsArray(0, y, xcount,ycount - y).astype(numpy.byte)
-            #datamask = bandmask.ReadAsArray(x-xoff, y-yoff,lineSize, ycount - (y-yoff)).astype(numpy.byte)
-
             dataraster = banddataraster.ReadAsArray(xoff, y+yoff,xcount, ycount - y).astype(numpy.byte)
             datamask = bandmask.ReadAsArray(0, y, xcount,ycount - y).astype(numpy.byte)
 
         zoneraster = dataraster[datamask==1]
 
-        #print(zoneraster)
-
-        #zoneraster = banddataraster.ReadAsArray(0, 0, xcount,ycount).astype(numpy.byte)
-        #print(numpy.sum(zoneraster))
         totalSum = totalSum + numpy.sum(zoneraster);
 
-        ##result.append(numpy.nanmean(zoneraster))
-        #result.append(stats.mode(zoneraster, axis=None)[0][0])
-
-        #print(stats.mode(zoneraster, axis=None)[0][0])
-
-        #print(totalSum*((pixelWidth)*(pixelWidth))/10000.0)
         dataraster = datamask = zoneraster = None
-            
-        #print('Total time spend: ' + str(datetime.datetime.now()-beginAt))
-    
-        #banddataraster = bandmask = raster = shp = target_ds = None
-
-        #uniques, counts = numpy.unique(dataraster, return_counts=True)
-
-        #for i,u in enumerate(uniques):
-        #    key = '0'+ str(u) if len(str(u)) == 1 else str(u)
-        #    if key not in result:
-        #        result[key] = 0
-        #    result[key] = result[key] + counts[i]
-        #resultStr = " "
-        #
-        #for key in range(1,4):
-        #    if str(key) in result:
-        #        resultStr = resultStr + str(result[str(key)]) + " "
-        #    else:
-        #        resultStr = resultStr + "0"
-        ##totalSum = totalSum + numpy.sum(zoneraster);
-        #
-
-
-        #dataraster = datamask = None
-        #resultStr = resultStr + "0 "
-
-        #uniques, counts = numpy.unique(zoneraster, return_counts=True)
-
-        #print(uniques,counts)
-
-        #for i,u in enumerate(uniques):
-        #
-        #    if (len(str(u))) == 1:
-        #        key = '0'+str(u)
-        #    else:
-        #        key = str(u)
-        #    if key not in result:
-        #        result[key] = 0
-        #    result[key] = result[key] + counts[i]
-
-        #keylist = list(result.keys())
-
-        #keylist.sort()
-
-        #for key in keylist:
-        #    print("{0}: {1}".format(key, result[key]))
-        
+   
     print('Total time spend: ' + str(datetime.datetime.now()-beginAt))
     
     banddataraster = bandmask = raster = shp = target_ds = None
 
-    #return result
-
-    #return stats.mode(result, axis=None)[0][0]
-
-    #return int(totalSum)
     return float(int(totalSum)*((pixelWidth)*(pixelHeight*-1))/10000.0)
 
 def loop_zonal_stats(input_zone_polygon, input_value_raster,field_name):
@@ -219,25 +148,14 @@ def loop_zonal_stats(input_zone_polygon, input_value_raster,field_name):
     statDict = {}
 
     for FID in featList:
-        #FID = 3
+
         feature = lyr.GetFeature(FID)
         name = feature.GetField(str(field_name))
-
-        #print(name)
-
-        #if name == 'Mata Atlântica':
 
         feat = lyr.GetFeature(FID)
         meanValue = zonal_stats(feat, input_zone_polygon, input_value_raster, FID)
            
         print('Biomas: ' + name,input_value_raster,meanValue)
-        #print(input_value_raster,meanValue)
-        
-        #keylist = list(meanValue.keys())
-        #keylist.sort()
-        #
-        #for key in keylist:
-        #    print("{0}: {1},".format(key, meanValue[key]),end =" ")
     
         statDict[name] = meanValue
     return statDict
