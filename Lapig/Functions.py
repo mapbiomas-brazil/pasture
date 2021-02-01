a5cba81854c90f0eed93c1c34ac9964a189ed57b
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2021-02-01 08:33:25 -0300

    Criado a funçao de login
    Foi criando uma função login para API do GEE

--- Lapig/Functions.py
+++ Lapig/Functions.py
@@ -1,3 +1,7 @@
+from dynaconf import settings
+from pathlib import Path
+
+
 def type_process(type_):
     if type_ in ['READY','RUNNING']:
         return 'RUNNING'
@@ -5,3 +9,18 @@ def type_process(type_):
         return 'COMPLETED'
     return 'IN_QUEUE'
     
+
+def login_gee(ee):
+    try:
+        ee.Initialize()
+    except FileNotFoundError as e:
+        __login_manual(ee)
+    except ee.ee_exception.EEException as e:
+        print('Login fall',e)
+        __login_manual(ee)
+        exit(1)
+
+
+def __login_manual(ee):
+    ee.Authenticate()
+    ee.Initialize()
